9a8d298b49ad4845bd519804c19e897b234f0d54
yasin elmas <m.yasinelmas@hotmail.com>
2021-08-02 08:23:10 +0300

    tamamlandı2

---
+++ RestFullService/ISunucu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestFullService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISunucu" in both code and config file together.
+    [ServiceContract]
+    public interface ISunucu
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        [WebGet(RequestFormat =WebMessageFormat.Json,ResponseFormat =WebMessageFormat.Json)]
+        string Deneme();
+    }
+}
